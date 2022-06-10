f0b93594e33af70e22f996caa51bc615cdafa40f
razielar <razielar@gmail.com>
2022-06-10 13:06:14 +0200

    init edge done

--- desktop/init_desktop.el
+++ desktop/init_desktop.el
@@ -1,6 +1,6 @@
 (org-babel-load-file
  (expand-file-name
-  "config.org"
+  "config_desktop.org"
   user-emacs-directory))
 
 
---
+++ edge_machine/init_edgeMachine.el
@@ -0,0 +1,6 @@
+(org-babel-load-file
+ (expand-file-name
+  "config_edgeMachine.org"
+  user-emacs-directory))
+
+
